dceacccb91071c962e6c82ac26c99ba9c90b99f1
Fespi Espino <florencia.espino@gmail.com>
2019-09-28 21:26:13 -0300

    vue tests + base sass

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
+Vue.component('games-item', {
+  props: ['games'],
+  template: '<li class="game">{{ games.text }}</li>'
+})
 
+var home = new Vue({
+  el: '#home',
+  data: {
+    message: 'estos son los juegos, buachin',
+     gamesList: [
+      { id: 0, text: 'Tapestry' },
+      { id: 1, text: 'Dinosaur Island' },
+      { id: 2, text: '7 Wonders' }
+    ]
+  }
+})
+
+
+/*
 
 var app = new Vue({
   el: '#app',
@@ -7,12 +25,4 @@ var app = new Vue({
   }
 })
 
-
-
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'lalala'
-  }
-})
+*/
