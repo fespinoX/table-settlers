a478d2c00cff21b81ad824381397aab129025622
Fespi Espino <florencia.espino@gmail.com>
2019-09-30 20:22:57 -0300

    cat list as test + vue local storage plug in

--- js/scripts.js
+++ js/scripts.js
@@ -7,22 +7,78 @@ var home = new Vue({
   el: '#home',
   data: {
     message: 'estos son los juegos, buachin',
+
      gamesList: [
+
       { id: 0, text: 'Tapestry' },
       { id: 1, text: 'Dinosaur Island' },
       { id: 2, text: '7 Wonders' }
+
     ]
   }
 })
 
 
-/*
 
-var app = new Vue({
+
+
+const app = new Vue({
   el: '#app',
   data: {
-    message: 'lalala'
+    cats: [
+    	{ 
+    		id: 0, 
+    		name: 'Zelda',
+    		superpower: 'muted miaws'
+    	},
+      	{ 
+      		id: 1, 
+      		name: 'Minerva',
+      		superpower: 'cuteness'
+      	},
+      	{ 
+      		id: 2, 
+      		name: 'Mariah',
+      		superpower: 'mini size'
+      	}
+    ],
+    newCat: null
+  },
+  mounted() {
+
+    if (localStorage.getItem('cats')) {
+      try {
+
+        // si ya existe cats en el localStorage, lo levanta
+        this.cats = JSON.parse(localStorage.getItem('cats'));
+      
+      } catch(e) {
+
+      	// si no existe, agrega data al localStorage
+        localStorage.removeItem('cats');
+        localStorage.setItem('cats', JSON.stringify(this.cats));
+        
+      }
+    } 
+  },
+  methods: {
+    addCat() {
+      // ensure they actually typed something
+      if (!this.newCat) {
+        return;
+      }
+
+      this.cats.push(this.newCat);
+      this.newCat = '';
+      this.saveCats();
+    },
+    removeCat(x) {
+      this.cats.splice(x, 1);
+      this.saveCats();
+    },
+    saveCats() {
+      const parsed = JSON.stringify(this.cats);
+      localStorage.setItem('cats', parsed);
+    }
   }
 })
-
-*/
